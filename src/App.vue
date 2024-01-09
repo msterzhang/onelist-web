bfa8ec878ecdab7bb82a87ca5267b428a51786a5
书小白 <2513002960@qq.com>
2024-01-09 08:41:34 +0800

    在影视详情页获取线上进度，这样不需要每次都刷新一下主页

--- src/App.vue
+++ src/App.vue
@@ -311,43 +311,6 @@ export default defineComponent({
 
         onMounted(() => {
             getConfig();
-            
-            const cookieValue = document.cookie
-                .split("; ")
-                .find((row) => row.startsWith("UserId" + "="));
-            console.log(cookieValue);
-            proxy.axios.get(proxy.COMMON.apiUrl + `/v1/api/progress/get`, {headers: {
-                    'content-type': 'application/json',
-                    'UserId': cookieValue.split("=")[1],
-                    'Authorization': proxy.$cookies.get("Authorization")
-                }}).then(res => {
-
-                console.log(res.data);
-                var times = {};
-                var a = localStorage.artplayer_settings != undefined?localStorage.artplayer_settings:"{}";
-                var local_times = JSON.parse(a).times;
-                if (local_times == undefined) local_times = {};
-                var server_times = JSON.parse(res.data.data).times;
-                if (server_times == undefined) server_times = {};
-                var times_keys = Array.from(new Set(Object.keys(local_times).concat(Object.keys(server_times))));
-                console.log(times_keys);
-                for (var key of times_keys) {
-                    // 两个都存在，对比本地与线上的进度，如果线上大于本地，则更新本地进度
-                    if (key in local_times && key in server_times){
-                        // 默认使用本地进度
-                        times[key] = local_times[key];
-                        if (local_times[key] < server_times[key]){
-                            times[key] = server_times[key];
-                        }
-                    }
-                    if (!(key in local_times) && key in server_times){
-                        times[key] = server_times[key];
-                    }
-
-                }
-                localStorage.artplayer_settings = JSON.stringify({"times": times});
-            }).catch((error) => {
-            });
         });
         return {
             dark,
